b2ed1a6bca4275aff280b7ab8b3c9d2696ca8bbf
Bárbara Ferreira <barbara_ferreira_3@hotmail.com>
2024-06-13 23:38:44 +0100

    Added turn off button

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,15 +4,21 @@ function App() {
   const [glow, setGlow] = useState("")
 
   return (
-    <div className='container'>
-      <div className={`red ${glow === "red-glow" ? "red-glow" : ""}`} onClick={
-        () => setGlow("red-glow")}></div>
+    <>
+      <div className='container'>
+        <div className={`red ${glow === "red-glow" ? "red-glow" : ""}`} onClick={
+          () => setGlow("red-glow")}></div>
 
-      <div className={`yellow ${glow === "yellow-glow" ? "yellow-glow" : ""}`} onClick={
-        () => setGlow("yellow-glow")}></div>
-      <div className={`green ${glow === "green-glow" ? "green-glow" : ""}`} onClick={
-        () => setGlow("green-glow")}></div>
-    </div>
+        <div className={`yellow ${glow === "yellow-glow" ? "yellow-glow" : ""}`} onClick={
+          () => setGlow("yellow-glow")}></div>
+        <div className={`green ${glow === "green-glow" ? "green-glow" : ""}`} onClick={
+          () => setGlow("green-glow")}></div>
+      </div>
+      <div className="buttons">
+        <button className="off" onClick={() => setGlow("")}>Turn off</button>
+      </div>
+    </>
+    
   )
 }
 
